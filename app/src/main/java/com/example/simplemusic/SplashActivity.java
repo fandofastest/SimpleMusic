ee822aecd2fafd9d55c7db8b5edb849432b26ee1
admin <fandofast@gmail.com>
2020-11-01 22:38:12 +0700

    aaaa

--- app/src/main/java/com/example/simplemusic/SplashActivity.java
+++ app/src/main/java/com/example/simplemusic/SplashActivity.java
@@ -67,7 +67,6 @@ public class SplashActivity extends AppCompatActivity {
                 Config.statusapp=response.getString("statusapp");
                 Config.appupdate=response.getString("appupdate");
                 Config.primaryads=response.getString("primaryads");
-                Config.statususer=response.getString("statususer");
 
                 if (Config.statusapp.equals("suspend")){
                     showDialog(Config.appupdate);
